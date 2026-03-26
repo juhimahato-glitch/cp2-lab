eb93b737690f2ab721133d363b558194b16477ec
JUHI <juhimahato081@gmail.com>
2026-03-26 23:16:03 +0530

    Add files via upload

---
+++ 6_8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i;
+    int even = 0, odd = 0;
+	printf("Enter number of elements: ");
+    scanf("%d", &n);
+	int arr[n];
+	printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+	int *ptr = arr;
+	for(i = 0; i < n; i++) {
+        if(*(ptr + i) % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+	printf("Number of even elements = %d\n", even);
+    printf("Number of odd elements = %d\n", odd);
+	return 0;
+}
